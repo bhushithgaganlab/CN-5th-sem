b5ebc7582115366d45dd054716b6566188b32100
bhushithgaganlab <42802614+bhushithgaganlab@users.noreply.github.com>
2020-11-06 15:56:16 +0530

    Delete testcsv.py

--- Week - 5/testcsv.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-
-path_to_csv= "/Users/nbvenkatesh88/Desktop/heatmapValuesSet1.csv"
-df = pd.read_excel(path_to_csv ,index_col=0)
-plt.imshow(df,cmap='hot',interpolation='nearest')
-plt.show()
